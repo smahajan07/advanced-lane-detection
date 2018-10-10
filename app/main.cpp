dc8b0577b02c455ee85cd5ea42e96b1594210fbb
smahajan07 <mahajan.sarthak28@gmail.com>
2018-10-09 21:45:19 -0400

    Completed Task #5 and #6
    To check if opencv is working on local machine, a test image was read and displayed. This is just to set base for openCV and make sure it works. Modifications will be done when the sprint for iteration 1 begins. This addition is just for reference and a new development branch will be created for the sprint. Build issue on travis will be resolved first before branch is created.

--- app/main.cpp
+++ app/main.cpp
@@ -8,5 +8,14 @@ int main()
 {
   std::cout<<"Trying to import opencv"<<std::endl;
   Mat img;
+  // Provide path for test image
+  std::string imgPath("image001.png");
+  // read test image
+  img = imread(imgPath, IMREAD_COLOR);
+  // create a window to display image that is read
+  namedWindow("Original", WINDOW_AUTOSIZE);
+  imshow("Original", img);
+  // wait for key press and then close window
+  waitKey(0);
   return 0;
 }
