a742ce3e2d9dee10eca6808ebf91aa2ca64bec09
smahajan07 <mahajan.sarthak28@gmail.com>
2018-10-12 04:50:07 -0400

    Reading from a video source and testing of new functionalities
    As per task #8 the frames are being read from a sample video and the pipeline is being test with new added functionalities #14 and #15.

--- app/main.cpp
+++ app/main.cpp
@@ -17,41 +17,71 @@ int main()
   lanedetector obj;
   Mat orgImg;
   // Provide path for test image
-  std::string imgPath("image001.png");
+  std::string vidPath("project_video.mp4");
   // read test image
-  orgImg = imread(imgPath, IMREAD_COLOR);
+//  orgImg = imread(imgPath, IMREAD_COLOR);
+  // open video
+  cv::VideoCapture cap(vidPath);
+  // check if video opened
+  if (!cap.isOpened())
+    return -1;
   // create a window to display image that is read
-  namedWindow("Original", WINDOW_AUTOSIZE);
-  imshow("Original", orgImg);
+//  namedWindow("Original", WINDOW_AUTOSIZE);
+//  imshow("Original", orgImg);
   // wait for key press and then close window
-  waitKey(0);
-  // actions
-  Mat undImg, ppImg, grayImg, edgeImg;
-  // call undistort func
-  undImg = obj.undistortImage(orgImg, cameraMatrix, distCoeff);
-  // call gray Image func
-  grayImg = obj.grayImage(undImg);
-  // call detect edges
-  edgeImg = obj.detectEdges(grayImg);
-  // call extract ROI
-  Mat roiImg;
-  int h = orgImg.rows, w = orgImg.cols;
-  Rect rectRoi(0.2 * w, h / 2, 0.6 * w, h / 2);
-  roiImg = obj.extractROI(edgeImg, rectRoi);
-  namedWindow("ROI image", WINDOW_AUTOSIZE);
-  imshow("ROI image", roiImg);
-  // wait for key press and then close window
-  waitKey(0);
-  // call perspective transform
-  Mat warpedImg;
-  warpedImg = obj.perspectiveTransform(edgeImg);
-  namedWindow("Warped image", WINDOW_AUTOSIZE);
-  imshow("Warped image", warpedImg);
-  // wait for key press and then close window
-  waitKey(0);
-  // call hough lines
-  std::vector<cv::Vec4i> lines;
-  lines = obj.detectLanes(warpedImg);
-
+//  waitKey(0);
+  int fCount = 0;
+  // start video loop
+  while (fCount < 500) {
+    if (!cap.read(orgImg))
+      break;
+    // actions
+    Mat undImg, ppImg, grayImg, edgeImg;
+    // call undistort func
+    undImg = obj.undistortImage(orgImg, cameraMatrix, distCoeff);
+    // call gray Image func
+    grayImg = obj.grayImage(undImg);
+    // call detect edges
+    edgeImg = obj.detectEdges(grayImg);
+    // call extract ROI
+    Mat roiImg;
+    int h = orgImg.rows, w = orgImg.cols;
+    Rect rectRoi(0.2 * w, h / 2, 0.6 * w, h / 2);
+    roiImg = obj.extractROI(edgeImg, rectRoi);
+    //  namedWindow("ROI image", WINDOW_AUTOSIZE);
+    //  imshow("ROI image", roiImg);
+    // wait for key press and then close window
+    //  waitKey(0);
+    // call create mask
+    Mat maskedImg;
+    maskedImg = obj.createMask(edgeImg);
+    //  namedWindow("Masked image", WINDOW_AUTOSIZE);
+    //  imshow("Masked image", maskedImg);
+    // wait for key press and then close window
+    //  waitKey(0);
+    // call perspective transform
+    Mat warpedImg;
+    warpedImg = obj.perspectiveTransform(edgeImg);
+    //  namedWindow("Warped image", WINDOW_AUTOSIZE);
+    //  imshow("Warped image", warpedImg);
+    // wait for key press and then close window
+    //  waitKey(0);
+    // call hough lines
+    std::vector<cv::Vec4i> lines;
+    lines = obj.detectLanes(maskedImg);
+    //  std::cout << "Original len " << lines.size() << std::endl;
+    // sort lines
+    std::vector<std::vector<cv::Vec4i>> validLines;
+    validLines = obj.sortLanes(lines, maskedImg);
+    // call draw lines
+    Mat linesImg;
+    linesImg = obj.drawLines(undImg, validLines[0], validLines[1]);
+//    namedWindow("Lines image", WINDOW_AUTOSIZE);
+//    imshow("Lines image", linesImg);
+    // wait for key press and then close window
+    //  waitKey(0);
+    ++fCount;
+    waitKey(15);
+  }
   return 0;
 }
